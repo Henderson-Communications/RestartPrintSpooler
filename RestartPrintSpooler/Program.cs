e3263544898f0b529c09bdc646142ded0882c7eb
Eric <eric@hendersoncommunications.net>
2020-12-18 10:19:10 -0500

    initial source upload

---
+++ RestartPrintSpooler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+
+namespace RestartPrintSpooler
+{
+    class Program
+    {
+        static bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        static void Main(string[] args)
+        {
+
+            if (!isWindows)
+            {
+                Console.WriteLine("This program restarts the print spooler on windows and does not support this platform.");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+
+            // Stop the spooler.
+            ServiceController service = new ServiceController("Spooler");
+            if ((!service.Status.Equals(ServiceControllerStatus.Stopped)) && 
+                (!service.Status.Equals(ServiceControllerStatus.StopPending))) {
+
+                Console.WriteLine("Stopping print spooler...");
+
+                service.Stop();
+                service.WaitForStatus(ServiceControllerStatus.Stopped);
+            }
+
+            // Start the spooler
+            Console.WriteLine("Restarting print spooler...");
+            service.Start();
+            service.WaitForStatus(ServiceControllerStatus.Running);
+
+            Console.WriteLine("Done. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+## RestartPrintSpooler
+
+RestartPrintSpooler is a simple tool to restart the print spooler on a windows machine without having to services to restart it.
